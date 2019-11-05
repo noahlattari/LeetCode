14d553f15dc7d30900d3abdf96b1df2a9e6433b5
noahlattari <zennoah1998@gmail.com>
2019-11-04 23:11:51 -0500

    d2d

--- src/Move_Zeroes_283.java
+++ src/Move_Zeroes_283.java
@@ -1,2 +1,16 @@
 public class Move_Zeroes_283 {
+    //https://leetcode.com/explore/interview/card/top-interview-questions-easy/92/array/567/
+    public void moveZeroes(int[] nums) {
+        int fast = 0;
+
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] != 0){
+                nums[fast++] = nums[i];
+            }
+        }
+
+        for(int i = fast; i < nums.length; i++){
+            nums[i] = 0;
+        }
+    }
 }

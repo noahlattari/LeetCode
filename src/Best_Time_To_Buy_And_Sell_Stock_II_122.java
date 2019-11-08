6d3e893bec22751116a70409b898ee6060ec70ff
noahlattari <zennoah1998@gmail.com>
2019-11-08 18:00:53 -0500

    fib

--- src/Best_Time_To_Buy_And_Sell_Stock_II_122.java
+++ src/Best_Time_To_Buy_And_Sell_Stock_II_122.java
@@ -12,6 +12,7 @@ public class Best_Time_To_Buy_And_Sell_Stock_II_122 {
                 res += prices[i] - prices[i-1];
             }
         }
+
         return res;
     }
 }
--- src/Fibonacci_Number_509.java
+++ src/Fibonacci_Number_509.java
@@ -1,4 +1,24 @@
-package PACKAGE_NAME;
+import java.util.*;
 
 public class Fibonacci_Number_509 {
+
+    public int fib(int N) {
+
+        if(N < 2){
+            return N;
+        }
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        if(map.containsKey(N)){
+            return map.get(N);
+        }else{
+            int sum = fib(N - 1) + fib(N - 2);
+            map.put(N, sum);
+            return sum;
+        }
+
+
+    }
+
+
 }

6c8bb6b6c5eb76377acac33f255c088288561a0d
noahlattari <zennoah1998@gmail.com>
2019-11-10 00:23:31 -0500

    pl;usone

--- src/Plus_One_66.java
+++ src/Plus_One_66.java
@@ -1,4 +1,18 @@
-package PACKAGE_NAME;
-
 public class Plus_One_66 {
+    public int[] plusOne(int[] digits) {
+
+        for(int i = digits.length -1; i >= 0; i--){
+            if(digits[i] != 9){
+                digits[i]++;
+                return digits;
+            }
+            digits[i] = 0;
+        }
+
+        int[] edge = new int[digits.length + 1];
+        edge[0] = 1;
+
+        return edge;
+
+    }
 }

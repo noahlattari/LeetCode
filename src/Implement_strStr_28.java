67d3dd8bd2afa93c6a7edf7b53abdb8603ed088f
noahlattari <zennoah1998@gmail.com>
2019-11-11 14:06:30 -0500

    vfs

--- src/Implement_strStr_28.java
+++ src/Implement_strStr_28.java
@@ -1,4 +1,21 @@
-package PACKAGE_NAME;
-
 public class Implement_strStr_28 {
+    public int strStr(String haystack, String needle) {
+        if(needle.isEmpty()) return 0;
+        if(needle == null || haystack == null) return -1;
+        // if(needle.length() > haystack.length()) return -1;
+        for(int i = 0; i < haystack.length() - needle.length() +1; i++){
+            if(haystack.charAt(i) == needle.charAt(0)){
+                int count = 0;
+                while(count < needle.length() && haystack.charAt(i+count) == needle.charAt(count)){
+                    count++;
+                }
+                if(count == needle.length()){
+                    return i;
+                }
+            }
+        }
+
+
+        return -1;
+    }
 }

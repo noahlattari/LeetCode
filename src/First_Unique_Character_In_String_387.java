6e7a795c06f545fe36b2724f4520cf282e42347c
noahlattari <zennoah1998@gmail.com>
2019-11-11 01:48:50 -0500

    imp

--- src/First_Unique_Character_In_String_387.java
+++ src/First_Unique_Character_In_String_387.java
@@ -1,4 +1,15 @@
-package PACKAGE_NAME;
-
+import java.util.*;
 public class First_Unique_Character_In_String_387 {
+    public int firstUniqChar(String s) {
+        HashMap<Character, Integer> map = new HashMap<>();
+        for(int i = 0; i < s.length(); i++){
+            map.put(s.charAt(i), map.getOrDefault(s.charAt(i), 0) + 1);
+        }
+        for(int i = 0; i < s.length(); i++){
+            if(map.get(s.charAt(i)) == 1){
+                return i;
+            }
+        }
+        return -1;
+    }
 }

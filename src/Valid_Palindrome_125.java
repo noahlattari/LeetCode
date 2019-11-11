d443e997f0f9f5bd557888c929ab59cfb0dd0fb7
noahlattari <zennoah1998@gmail.com>
2019-11-11 12:31:40 -0500

    code

--- src/Valid_Palindrome_125.java
+++ src/Valid_Palindrome_125.java
@@ -1,4 +1,28 @@
-package PACKAGE_NAME;
-
 public class Valid_Palindrome_125 {
+    public boolean isPalindrome(String s) {
+        if(s.length() == 0 || s == null){
+            return true;
+        }
+
+        s = s.toLowerCase();
+
+        int start = 0;
+        int end = s.length() -1;
+
+        while(start < end){
+            while( (start < end) && (!Character.isLetterOrDigit(s.charAt(start))) ){
+                start++;
+            }
+            while( (start < end) && (!Character.isLetterOrDigit(s.charAt(end))) ){
+                end--;
+            }
+            if(s.charAt(start) != s.charAt(end)){
+                return false;
+            }
+            start++; end--;
+        }
+
+        return true;
+
+    }
 }

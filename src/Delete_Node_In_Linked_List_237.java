13994f20e44c7961100f97d788c95c77a2eb69c7
noahlattari <zennoah1998@gmail.com>
2019-11-13 18:01:59 -0500

    ?

--- src/Delete_Node_In_Linked_List_237.java
+++ src/Delete_Node_In_Linked_List_237.java
@@ -1,4 +1,8 @@
-package PACKAGE_NAME;
-
 public class Delete_Node_In_Linked_List_237 {
+    class Solution {
+        public void deleteNode(ListNode node) {
+            node.next = node.next.next;
+            node.val = node.next.val;
+        }
+    }
 }

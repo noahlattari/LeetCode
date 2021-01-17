88305dacfff057889c5775ef12a60caf2cc4b65a
Noah Mifsud Lattari <zennoah1998@gmail.com>
2021-01-17 15:01:59 -0500

    Time: 1 ms (49.74%), Space: 38.6 MB (91.69%) - LeetHub

---
+++ maximum-subarray/maximum-subarray.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int maxSubArray(int[] A) {
+        int[] memo = new int[A.length];
+        memo[0] = A[0];
+        int maxSum = memo[0];
+        
+        for(int i = 1; i < A.length; i++){
+            memo[i] = Math.max(A[i], memo[i-1] + A[i]);
+            maxSum = Math.max(memo[i], maxSum);
+        }
+        return maxSum;
+    }
+}

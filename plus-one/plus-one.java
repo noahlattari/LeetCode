d0dec80231d9a34d1d57412d7f6aadf1c95df211
Noah Mifsud Lattari <zennoah1998@gmail.com>
2021-01-17 15:19:17 -0500

    Time: 0 ms (100.00%), Space: 39 MB (8.80%) - LeetHub

---
+++ plus-one/plus-one.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] plusOne(int[] digits) {
+        
+        for(int i = digits.length -1; i >= 0; i--){
+            if(digits[i] != 9){
+                digits[i]++;
+                return digits;
+            }else{
+                digits[i] = 0;
+            }
+        }
+        
+        int[] edge = new int[digits.length + 1];
+        edge[0] = 1;
+        return edge;
+        
+    }
+}

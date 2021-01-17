f43838d2ed17d32069fa92dcaea9e173fa506cf5
Noah Mifsud Lattari <zennoah1998@gmail.com>
2021-01-17 17:15:10 -0500

    Time: 0 ms (100.00%), Space: 39.4 MB (22.39%) - LeetHub

---
+++ rotate-image/rotate-image.java
@@ -0,0 +1,26 @@
+class Solution {
+    public void rotate(int[][] matrix) {
+        
+        //swap rows
+        int start = 0;
+        int end = matrix.length -1;
+        
+        while(start < end){
+            int[] temp = matrix[start];
+            matrix[start] = matrix[end];
+            matrix[end] = temp;
+            start++; end--;
+        }
+        
+        //transpose
+        for(int r = 0; r < matrix.length; r++){
+            for(int c = r + 1; c < matrix[r].length; c++){
+                int temp = matrix[r][c];
+                matrix[r][c] = matrix[c][r];
+                matrix[c][r] = temp;
+            }
+        }
+        
+        
+    }
+}
